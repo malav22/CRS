015562f25f0268569bff915c70b0a1cbdf22f042
malav22 <malavakhani58@gmail.com>
2023-04-12 20:32:45 +0530

    final

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+
+
+<footer>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 text-right">
+          <div class="footer_widget">
+            <p>Made by Malav, Lay, Lakshya, Bharath and Devi</p>
+            <ul>
+              
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          <p class="copy-right">Copyright &copy; 2022 Car Rental System.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
